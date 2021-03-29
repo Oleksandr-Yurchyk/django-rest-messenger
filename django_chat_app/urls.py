12162ac7473b40bdb7aae128ca27c8d61f38dcb3
Oleksandr Yurchyk <yurchyk_sasha@ukr.net>
2021-03-29 17:36:02 +0300

    minor changes

--- django_chat_app/urls.py
+++ django_chat_app/urls.py
@@ -35,5 +35,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/messages/', include('api_messenger.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
